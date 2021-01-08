fadabd3a74473d30e37ec0cf97c6bec176a86b7b
Ricardo Elías <ricardoportillo14@gmail.com>
2021-01-08 17:00:38 -0600

    Ultimos detalles en el manejo de BT

--- app/src/main/java/com/example/arduinobt/DispositivosVinculados.java
+++ app/src/main/java/com/example/arduinobt/DispositivosVinculados.java
@@ -2,15 +2,102 @@ package com.example.arduinobt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Set;
 
 public class DispositivosVinculados extends AppCompatActivity
 {
+    private static final String TAG = "DispositivosVinculados";
+    public static String EXTRA_DEVICE_ADRESS = "device_adress";
+
+    private BluetoothAdapter BTAdapter;
+
+    private ArrayAdapter ArrayBT;
+
+    ListView ListaBT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispositivos_vinculados);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        VerificarStadoBT();
+
+        ArrayBT = new ArrayAdapter(this, R.layout.dispositivos_encontrados);
+
+        ListaBT = findViewById(R.id.ListaBT);
+
+        ListaBT.setAdapter(ArrayBT);
+
+        ListaBT.setOnItemClickListener(DeviceListener);
+
+        BTAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Set<BluetoothDevice> pairedDevices = BTAdapter.getBondedDevices();
+
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device: pairedDevices)
+            {
+                ArrayBT.add(device.getName() +"\n" +device.getAddress());
+            }
+        }
+    }
+
+    private AdapterView.OnItemClickListener DeviceListener = new AdapterView.OnItemClickListener()
+    {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            String info = ((TextView) view).getText().toString();
+            String Adress = info.substring(info.length() - 17);
+
+            finishAffinity();
+
+            Intent OpenNewActivity = new Intent(getApplicationContext(), MainActivity.class);
+            OpenNewActivity.putExtra(EXTRA_DEVICE_ADRESS , Adress);
+            startActivity(OpenNewActivity);
+        }
+    };
+
+    public void VerificarStadoBT()
+    {
+        BTAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(BTAdapter == null)
+        {
+            Toast.makeText(getApplicationContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            if(BTAdapter.isEnabled())
+            {
+                 Toast.makeText(getApplicationContext(), "Bluetooth activado", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Intent EnableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(EnableBT, 1);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/arduinobt/MainActivity.java
+++ app/src/main/java/com/example/arduinobt/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.arduinobt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -15,6 +24,19 @@ public class MainActivity extends AppCompatActivity
     private TextView Estado;
     private EditText IngresoTxt;
     private ImageButton Izquierda, Derecha, Claxon, Acelerador, Freno;
+
+    Handler BTIn;
+    final int HandlerState = 0;
+    private BluetoothAdapter BTAdapter = null;
+    private BluetoothSocket BTSocket = null;
+    private StringBuilder DataStringIn = new StringBuilder();
+
+    private ConnectedThread MyBTCnx;
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    private static String Adress = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -22,6 +44,16 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BTIn = new Handler()
+        {
+            public void handleMessage(android.os.Message msg)
+            {
+                if(msg.what == handlerState)
+                {
+
+                }
+            }
+        };
 
         Conectar = findViewById(R.id.Conectar);
         Desconectar = findViewById(R.id.Desconectar);
@@ -38,7 +70,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                
+
             }
         });
 
@@ -47,7 +79,18 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-
+                if(BTSocket != null)
+                {
+                    try
+                    {
+                        BTSocket.close();
+                    }
+                    catch (IOException e)
+                    {
+                        Toast.makeText(getApplicationContext(), "Error" , Toast.LENGTH_SHORT).show();
+                    }
+                }
+                finish();
             }
         });
 
@@ -95,14 +138,83 @@ public class MainActivity extends AppCompatActivity
 
             }
         });
-
-        Desconectar.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-
-            }
-        });
     }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException
+    {
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        Intent intent = getIntent();
+
+        Adress = intent.getStringExtra(DispositivosVinculados.EXTRA_DEVICE_ADRESS);
+
+        BluetoothDevice device = BTAdapter.getRemoteDevice(Adress);
+
+        try
+        {
+            BTSocket = createBluetoothSocket(device);
+        }
+        catch (IOException e)
+        {
+            Toast.makeText(getBaseContext(), "La creaccion del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+
+        try
+        {
+            BTSocket.connect();
+        }
+        catch (IOException e)
+        {
+            try
+            {
+                BTSocket.close();
+            } catch (IOException e2) {}
+        }
+
+        MyBTCnx = new ConnectedThread(BTSocket);
+        MyBTCnx.start();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        try
+        {
+            BTSocket.close();
+        }catch (IOException e2) {}
+    }
+
+    public void VerificarStadoBT()
+    {
+        if(BTAdapter == null)
+        {
+            Toast.makeText(getApplicationContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            if(BTAdapter.isEnabled())
+            {
+                Toast.makeText(getApplicationContext(), "Bluetooth activado", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Intent EnableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(EnableBT, 1);
+            }
+        }
+    }
+
+    private class ConnectedThread(BluetoothSocket socket)
+    {
+        ///---11:37 P2
+    }
+
 }
